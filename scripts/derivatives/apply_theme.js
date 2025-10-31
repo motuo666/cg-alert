9345306f04460330285923e67ed4d5696f735829
motuo666 <szning1117@gmail.com>
2025-10-31 11:51:52 +0800

    Create apply_theme.js

---
+++ scripts/derivatives/apply_theme.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const fs = require('fs'); const path = require('path');
+const CANON = 'https://www.cg-alert.com/';
+const header = fs.readFileSync(path.join('partials','header.html'),'utf8');
+const footer = fs.readFileSync(path.join('partials','footer.html'),'utf8');
+const headAdd = `
+<link rel="stylesheet" href="/assets/cg.css">
+<link rel="alternate" type="application/rss+xml" title="CG Alert Reports" href="/reports/rss.xml">
+<link rel="canonical" href="${CANON}">
+`;
+function* allHtml(){
+  function walk(p){
+    for(const name of fs.readdirSync(p)){
+      const fp = path.join(p,name);
+      if(name.startsWith('.') || name==='node_modules' || name==='.git' || name==='assets') continue;
+      const st = fs.statSync(fp);
+      if(st.isDirectory()) walk(fp);
+      else if(/\.html$/i.test(name)) yield fp;
+    }
+  }
+  walk('.');
+}
+let touched=0;
+for(const fp of allHtml()){
+  let html = fs.readFileSync(fp,'utf8');
+  if(!/\/assets\/cg\.css/.test(html)){
+    html = html.replace(/<head(.*?)>/i, (m)=> m + headAdd);
+  }
+  if(!/<header[\s>]/i.test(html)){
+    html = html.replace(/<body(.*?)>/i, (m)=> m + "\n" + header);
+  }
+  if(!/<footer[\s>]/i.test(html)){
+    html = html.replace(/<\/body>/i, footer + "\n</body>");
+  }
+  fs.writeFileSync(fp, html, 'utf8'); touched++;
+}
+console.log('apply_theme(light): updated files', touched);
