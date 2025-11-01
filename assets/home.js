125f7484dbfe9a51444476d2554385a8c0cf2c86
motuo666 <szning1117@gmail.com>
2025-11-01 13:55:13 +0800

    Add files via upload

--- assets/home.js
+++ assets/home.js
@@ -1,1 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const id=a.getAttribute('href').slice(1);const el=document.getElementById(id);if(!el)return; e.preventDefault(); el.scrollIntoView({behavior:'smooth'});}));
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const id=a.getAttribute('href').slice(1);const el=document.getElementById(id);if(!el)return;e.preventDefault();el.scrollIntoView({behavior:'smooth'});}));
