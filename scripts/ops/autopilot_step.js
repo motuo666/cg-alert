410cfede3624b716656e39d495a665876bf7d88a
motuo666 <szning1117@gmail.com>
2025-11-25 23:38:09 +0800

    Delete scripts/ops/autopilot_step.js

--- scripts/ops/autopilot_step.js
+++
@@ -1,5 +0,0 @@
-
-#!/usr/bin/env node
-const { spawnSync } = require('node:child_process');
-const r = spawnSync('node', ['scripts/ops/autopilot_step.mjs'], { stdio: 'inherit' });
-process.exit(r.status || 0);
