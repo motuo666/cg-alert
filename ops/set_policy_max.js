4fba3f147e0c011a66175f478d09597fc85d2445
motuo666 <szning1117@gmail.com>
2025-11-25 17:40:28 +0800

    Delete ops/set_policy_max.js

--- ops/set_policy_max.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const fs=require('fs');
-const path='config/volume_policy.json';
-const target=parseInt(process.argv[2]||'100',10);
-const j=JSON.parse(fs.readFileSync(path,'utf8'));
-j.max=target;
-fs.writeFileSync(path, JSON.stringify(j,null,2)+'\n');
-console.log('set max ->', target);
